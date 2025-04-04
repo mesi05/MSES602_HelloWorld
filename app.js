afedc7ba45baaa6ddb3e313f9fb2cdc5302d4295
William Masters <rmesidor@regis.edu>
2025-04-04 13:42:06 -0700

    Changed name to William Masters

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const hostname = '127.0.0.1';
 const port = 8082;
 
 const server = http.createServer((req, res) => {
+	  console.log(`${req.method} ${req.url}`); // To Log each request
 	  res.statusCode = 200;
 	  res.setHeader('Content-Type', 'text/plain');
-	  res.end('Hello World. My name is Vincent Danh');
+	  res.end('Hello World. My name is William (Bill) Masters');
 });
 
 server.listen(port, hostname, () => {
